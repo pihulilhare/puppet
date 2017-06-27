0794357242b9cf0d55b34bea0c326151e679a4e6
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-27 12:31:08 +0530

    Update FileCreate.pp

--- FileCreate.pp
+++ FileCreate.pp
@@ -1,1 +1,7 @@
-
+file { '/root/First_File.txt':
+  ensure => "file",
+  owner  => "root",
+  group  => "root",
+  mode   => "755",
+  content => "Hello..!! Welcome to d world of PUPPET",
+  }                                                                                                               ~    
