5d0c84369aae279127372c1fb2500216873b5258
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-27 14:25:01 +0530

    Update InstallGit.pp

--- InstallGit.pp
+++ InstallGit.pp
@@ -1,3 +1,5 @@
+
+
 #4. Write a script which will configure a git and clone some repository through it.
 
 include git
@@ -16,3 +18,23 @@ vcsrepo { '/tmp/git':
   #branch   => 'v2.2.0',
   #depth    => 1,
 }
+''' output
+
+[priyanka_lilhare@client ~]$ sudo -i
+[root@client ~]# ls
+First_File.txt
+[root@client ~]# cd /tmp
+[root@client git]# git config --list
+user.email=priyanka.lilhare@gmail.com
+user.name=pihulilhare
+core.repositoryformatversion=0 
+core.filemode=true 
+core.bare=false
+core.logallrefupdates=true
+remote.origin.url=https://github.com/pihulilhare/puppet.gitremote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+branch.master.remote=origin
+branch.master.merge=refs/heads/master
+[root@client git]# ls
+ileCreate.pp  groupcreate.pp  InstallGit.pp  site.pp           
+usercreate.ppFileDelete.pp  groupdelete.pp  README.md     
+tomcatinstall.pp  userdelete.pp'''
