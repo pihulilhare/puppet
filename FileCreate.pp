1d9cf5658d3c6205a43223a6e97759902b62c0a5
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-27 16:46:59 +0530

    Update FileCreate.pp

--- FileCreate.pp
+++ FileCreate.pp
@@ -1,7 +1,17 @@
+#File Creation
 file { '/root/First_File.txt':
   ensure => "file",
   owner  => "root",
   group  => "root",
   mode   => "755",
   content => "Hello..!! Welcome to d world of PUPPET",
-  }                                                                                                               ~    
+  } 
+  '''OUTPUT
+  [root@client ~]# puppet agent -t
+  Info: Retrieving pluginfacts
+  Info: Retrieving pluginInfo: Loading facts
+  Info: Caching catalog for client.c.api-project-931139647699.internal
+  Info: Applying configuration version '1498560943'Notice: Finished catalog run in 0.17 seconds
+  
+  [root@client ~]# ls
+  First_File.txt
