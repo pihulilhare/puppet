ae86d17dfe033d147f38c3a58aa7f4a810f64b15
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-27 16:02:03 +0530

    Update tomcatinstall.pp

--- tomcatinstall.pp
+++ tomcatinstall.pp
@@ -1,4 +1,5 @@
 
+
 #Write a script which do the installation of tomcat server and deploy a war file through it.
 #puppet module install puppetlabs/java
 # In site.pp
@@ -21,4 +22,19 @@ tomcat::war { 'warfile.war':
   catalina_base => '/opt/tomcat',
   war_source    => '/opt/tomcat/webapps/docs/appdev/sample/warfile.war',
 }
+
+''' output
+[root@client tomcat]# ls
+bin  conf  lib  LICENSE  logs  NOTICE  RELEASE-NOTES  RUNNING.txt  temp  webapps  work
+[root@client tomcat]# cd bin
+[root@client bin]#     sh startup.sh
+Using CATALINA_BASE:   /opt/tomcat
+Using CATALINA_HOME:   /opt/tomcat
+Using CATALINA_TMPDIR: /opt/tomcat/temp
+Using JRE_HOME:        /usr/lib/jvm/java-1.8.0-openjdk-x86_64/
+Using CLASSPATH:       /opt/tomcat/bin/bootstrap.jar:/opt/tomcat/bin/tomcat-juli.jar
+Tomcat started.
+
+
+[root@client bin]# curl localhost:8080'''
 
