e932ae2fdea72accde58d51a022107da5049e0b6
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-27 16:54:27 +0530

    Update usercreate.pp

--- usercreate.pp
+++ usercreate.pp
@@ -1,3 +1,4 @@
+
 # script for creating user
 user { 'priya': 
    ensure     => 'present', 
@@ -5,3 +6,16 @@ user { 'priya':
    shell      => '/bin/bash', 
    managehome => 'true', 
 }
+'''output
+[root@client ~]# id priya
+id: priya: no such user
+[root@client ~]# puppet agent -t 
+   Info: Retrieving pluginfacts 
+   Info: Retrieving pluginInfo: Loading facts 
+   Info: Caching catalog for client.c.api-project-931139647699.internal 
+   Info: Applying configuration version '1498560943'Notice: Finished catalog run in 0.17 seconds 
+[root@client ~]# id priya
+uid=501(priya) gid=1003(priya) groups=1003(priya)'''
+
+
+
